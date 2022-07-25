6da8c94810c575528cf10c845cdf470ea3f6a292
Saransh <17107201s@gamil.com>
2022-07-25 14:00:17 +0530

    added bcrypt

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const md5 = require('md5');
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs =require("ejs");
-const { urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app=express();
 
@@ -33,15 +33,17 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if(err)console.log(err);
-        else{
-            res.render("secrets");
-        }
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save(function(err){
+            if(err)console.log(err);
+            else{
+                res.render("secrets");
+            }
+        });
     });
 });
 
@@ -50,9 +52,9 @@ app.post("/login",function(req,res){
         if(err)console.log(err);
         else{
             if(foundUser){
-                if(foundUser.password===md5(req.body.password)){
-                    res.render("secrets");
-                }
+                bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+                    if(result===true)res.render("secrets"); 
+                });
             }
         }
     });
