c95a8d64f7da9ea50a7c45ad9144953c7b3dabdd
Saransh <17107201s@gamil.com>
2022-07-25 13:42:39 +0530

    added md5

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
+const md5 = require('md5');
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs =require("ejs");
 const { urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
 
 const app=express();
 
@@ -17,8 +17,6 @@ const userSchema= new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password']});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -37,7 +35,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err)console.log(err);
@@ -52,7 +50,7 @@ app.post("/login",function(req,res){
         if(err)console.log(err);
         else{
             if(foundUser){
-                if(foundUser.password===req.body.password){
+                if(foundUser.password===md5(req.body.password)){
                     res.render("secrets");
                 }
             }
